b09ea9633d46b5a7d7274562f532021e43e095d5
Rodel Dahay <odedahay@gmail.com>
2026-08-17 02:17:00 -0600

    corrected the functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,30 @@
 <?php
 
 function calgarybrightminds_files() {
-    wp_enqueue_style('calgarybrightminds_main_style', get_theme_file_uri( '/css/styles.css' ));
-    wp_enqueue_style('calgarybrightminds-fonts','https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&display=swap',array(),null);
-
-    wp_enqueue_style('swiper','https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css', array(), '12');
-
-    wp_enqueue_style('odometer','https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.8/themes/odometer-theme-default.min.css', array(),'0.4.8');
+    wp_enqueue_style(
+        'calgarybrightminds-fonts',
+        'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&display=swap',
+        array(),
+        null
+    );
 
     wp_enqueue_style(
-        'calgarybrightminds-main', get_theme_file_uri('/css/styles.css'), 
+        'swiper',
+        'https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css',
+        array(),
+        '12'
+    );
+
+    wp_enqueue_style(
+        'odometer',
+        'https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.8/themes/odometer-theme-default.min.css',
+        array(),
+        '0.4.8'
+    );
+
+    wp_enqueue_style(
+        'calgarybrightminds-main',
+        get_theme_file_uri('/css/styles.css'),
         array('calgarybrightminds-fonts', 'swiper', 'odometer'),
         filemtime(get_theme_file_path('/css/styles.css'))
     );
@@ -37,7 +52,6 @@ function calgarybrightminds_files() {
         filemtime(get_theme_file_path('/js/scripts.js')),
         true
     );
-  
 }
 
 add_action('wp_enqueue_scripts', 'calgarybrightminds_files');
@@ -55,6 +69,5 @@ function calgarybrightminds_resource_hints($urls, $relation_type) {
 }
 
 add_filter('wp_resource_hints', 'calgarybrightminds_resource_hints', 10, 2);
-
 
 
